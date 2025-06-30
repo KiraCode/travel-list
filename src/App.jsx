90f50602a0822fca8cd3324004dd0a7925da8c1b
no name <your-email@example.com>
2025-06-30 16:24:20 +0530

    added functionality to calculate items that is packed and not packed

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -116,12 +116,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if(!items.length){
+    return(
+      <p className="stats">
+        <em>Start adding some items in your packing</em>
+      </p>
+    )
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X(x%)</em>
+      <em>
+        {percentage === 100
+          ? "You get everything! Ready to go ✈"
+          : `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
-
-// export default App;
